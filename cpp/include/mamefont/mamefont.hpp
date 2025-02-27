f21e96c62741cbc4fef1cf600f990f0e09d75c1a
shapoco <40152459+shapoco@users.noreply.github.com>
2025-02-27 13:57:53 +0900

    wip

--- cpp/include/mamefont/mamefont.hpp
+++ cpp/include/mamefont/mamefont.hpp
@@ -2,11 +2,17 @@
 
 #include <stdint.h>
 
+#ifndef ALWAYS_INLINE
+#define ALWAYS_INLINE __attribute__((always_inline))
+#endif
+
 namespace mfnt {
 
 static constexpr uint16_t GLYPH_OFFSET_DUMMY = 0xffffu;
 static constexpr uint8_t SEGMENT_HEIGHT = 8;
 static constexpr uint8_t HISTORY_SIZE = 32;
+
+#define MFNT_SUPPORT_RESUME (0)
 
 enum class Status {
   SUCCESS = 0,
@@ -27,39 +33,54 @@ struct ExtractContext {
   uint16_t end;
   uint16_t wrPos;
   uint8_t last;
+#if MFNT_SUPPORT_RESUME
   uint8_t *history;
+#endif
   const uint8_t *rdPtr;
 
-  void init(GlyphInfo *glyph, uint8_t *buff, uint16_t start = 0, uint16_t end = 0xffff, uint8_t *history = nullptr) {
+  void init(GlyphInfo *glyph, uint8_t *buff, uint16_t start = 0,
+            uint16_t end = 0xffff
+#if MFNT_SUPPORT_RESUME
+            ,
+            uint8_t *history = nullptr
+#endif
+  ) {
     this->glyph = glyph;
     this->buff = buff;
     this->start = start;
     this->end = end;
     this->wrPos = 0;
     this->last = 0;
+#if MFNT_SUPPORT_RESUME
     this->history = history;
+#endif
     this->rdPtr = glyph->data;
   }
-  
+
   void write(uint8_t value) {
     if (start <= wrPos && wrPos < end) {
       buff[wrPos++] = value;
     }
+#if MFNT_SUPPORT_RESUME
     if (history) {
       history[wrPos & (HISTORY_SIZE - 1)] = value;
     }
+#endif
     this->last = value;
   }
 
   uint8_t read(uint16_t pos) const {
+#if MFNT_SUPPORT_RESUME
     if (history) {
       return history[pos & (HISTORY_SIZE - 1)];
-    }
-    else {
+    } else {
       return buff[pos];
     }
+#else
+    return buff[pos];
+#endif
   }
-};
+};  // namespace mfnt
 
 static inline void shiftOp(ExtractContext *ctx, uint8_t inst) {
   uint8_t last = ctx->last;
@@ -72,8 +93,7 @@ static inline void shiftOp(ExtractContext *ctx, uint8_t inst) {
       if (shift_dir == 0) {
         last = (last << 1) & 0xfe;
         if (post_op) last |= 0x01;
-      }
-      else {
+      } else {
         last = (last >> 1) & 0x7f;
         if (post_op) last |= 0x80;
       }
@@ -105,7 +125,7 @@ static inline void xorOp(ExtractContext *ctx, uint8_t inst) {
 }
 
 class Font {
-public:
+ public:
   const uint8_t height;
   const uint8_t rows;
   const uint8_t numChars;
@@ -115,23 +135,17 @@ public:
   const uint8_t *segTable;
   const uint8_t *glyphData;
 
-  Font(
-    const uint8_t height, 
-    const uint8_t numChars,
-    const uint8_t codeOffset, 
-    const uint8_t flags, 
-    const uint16_t *charTable, 
-    const uint8_t *segTable, 
-    const uint8_t *glyphData
-  ) : 
-    height(height), 
-    rows((height + SEGMENT_HEIGHT - 1) / SEGMENT_HEIGHT),
-    numChars(numChars), 
-    codeOffset(codeOffset),
-    flags(flags),
-    charTable(charTable), 
-    segTable(segTable), 
-    glyphData(glyphData) {}
+  Font(const uint8_t height, const uint8_t numChars, const uint8_t codeOffset,
+       const uint8_t flags, const uint16_t *charTable, const uint8_t *segTable,
+       const uint8_t *glyphData)
+      : height(height),
+        rows((height + SEGMENT_HEIGHT - 1) / SEGMENT_HEIGHT),
+        numChars(numChars),
+        codeOffset(codeOffset),
+        flags(flags),
+        charTable(charTable),
+        segTable(segTable),
+        glyphData(glyphData) {}
 
   Status getGlyph(uint8_t c, GlyphInfo *glyph) const {
     uint8_t index = c;
@@ -159,7 +173,7 @@ public:
     return maxWidth;
   }
 
-  uint16_t getGlyphSizeInBytes(const GlyphInfo* glyph) const {
+  uint16_t getGlyphSizeInBytes(const GlyphInfo *glyph) const {
     return glyph->width * rows;
   }
 
@@ -170,36 +184,36 @@ public:
     while (ctx->wrPos < ctx->end) {
       uint8_t inst = *(ctx->rdPtr++);
       switch (inst & 0xf0) {
-        case 0x00: // LD
-        case 0x10: // LD
-        case 0x20: // LD
-        case 0x30: // LD
+        case 0x00:  // LD
+        case 0x10:  // LD
+        case 0x20:  // LD
+        case 0x30:  // LD
           ctx->last = ctx->buff[ctx->wrPos++] = segTable[inst & 0x3f];
           break;
-        
-        case 0x40: // SLC
-        case 0x50: // SLS
-        case 0x60: // SRC
-        case 0x70: // SRS
+
+        case 0x40:  // SLC
+        case 0x50:  // SLS
+        case 0x60:  // SRC
+        case 0x70:  // SRS
           shiftOp(ctx, inst);
           break;
-        
-        case 0x80: // CPY
-        case 0x90: // CPY
-        case 0xa0: // CPY
-        case 0xb0: // CPY
+
+        case 0x80:  // CPY
+        case 0x90:  // CPY
+        case 0xa0:  // CPY
+        case 0xb0:  // CPY
           copyOp(ctx, inst);
           break;
-        
-        case 0xc0: // REV
-        case 0xd0: // REV
+
+        case 0xc0:  // REV
+        case 0xd0:  // REV
           return Status::UNKNOWN_OPCODE;
-        
-        case 0xe0: // RPT
+
+        case 0xe0:  // RPT
           repeatOp(ctx, inst);
           break;
 
-        case 0xf0: // XOR
+        case 0xf0:  // XOR
           if (inst == 0xff) return Status::UNKNOWN_OPCODE;
           xorOp(ctx, inst);
           break;
@@ -220,7 +234,5 @@ public:
     if (width) *width = glyph.width;
     return extractGlyph(&ctx);
   }
-  
 };
-
-}
+}  // namespace mfnt
